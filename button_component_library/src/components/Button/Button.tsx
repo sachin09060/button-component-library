e71ada95a5c06e57e0f96cc841293bd4604936b4
Sachin T P. <sachin.tp@excelindia.com>
2024-09-11 14:20:05 +0530

    component ui updated

--- button_component_library/src/components/Button/Button.tsx
+++ button_component_library/src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 const Button = (props: ButtonProps) => {
   return (
     <div>
-        <button className='btn'>{props.label}</button>
+        <button className='btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>{props.label}</button>
     </div>
   )
 }
